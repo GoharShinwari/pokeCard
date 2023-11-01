dbd79c0a5137168941fa7d195c3ed2f390e3b898
GoharShinwari <121142075+GoharShinwari@users.noreply.github.com>
2023-11-01 13:20:41 -0400

    fixed up some speed issues

--- code.js
+++ code.js
@@ -1,31 +1,35 @@
-function searchCard() {
-    const cardName = document.getElementById('cardName').value;
-    const apiUrl = `https://api.pokemontcg.io/v2/cards?q=name:${cardName}&pageSize=1000`;
-  
-    // API Info is fetched, card info is created and sorted by 6 in rows
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        let cardInfo = '';
-        data.data.forEach((cardData, index) => {
-          if (index % 6 === 0) {
-            cardInfo += '<div class="row">';
-          }
-          cardInfo += `
-            <div class="card">
-              <div class="card-img">
-                <img src="${cardData.images.small}">
-              </div>
-              <div class="card-info">
-              </div>
-            </div>
-          `;
-          if ((index + 1) % 6 === 0 || index === data.data.length - 1) {
-            cardInfo += '</div>';
-          }
-        });
-        document.getElementById('cardInfo').innerHTML = cardInfo;
-      })
-      .catch(error => console.error(error));
+function searchCard(event) {
+  event.preventDefault(); // Prevent form submission
+  const cardName = document.getElementById('cardName').value;
+  if (cardName) {
+      const apiUrl = `https://api.pokemontcg.io/v2/cards?q=name:${cardName}&pageSize=1000`;
+
+      // API Info is fetched, card info is created and sorted by 6 in rows
+      fetch(apiUrl)
+          .then(response => response.json())
+          .then(data => {
+              let cardInfo = '';
+              data.data.forEach((cardData, index) => {
+                  if (index % 6 === 0) {
+                      cardInfo += '<div class="row">';
+                  }
+                  cardInfo += `
+                      <div class="card">
+                          <div class="card-img">
+                              <img src="${cardData.images.small}">
+                          </div>
+                          <div class="card-info">
+                          </div>
+                      </div>
+                  `;
+                  if ((index + 1) % 6 === 0 || index === data.data.length - 1) {
+                      cardInfo += '</div>';
+                  }
+              });
+              document.getElementById('cardInfo').innerHTML = cardInfo;
+          })
+          .catch(error => console.error(error));
   }
-  
+}
+
+document.querySelector('form').addEventListener('submit', searchCard);
